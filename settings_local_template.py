09d5a05193c0b1fc829791b9039522c3e5574e90
AJ <a@ajweb.eu>
2015-05-23 19:44:32 +0200

    Updated requirements and settings template

--- settings_local_template.py
+++ settings_local_template.py
@@ -2,6 +2,14 @@ TWITTER_CONSUMER_KEY = ''
 TWITTER_CONSUMER_SECRET = ''
 
 
-# if your adding this file locally, you probably want to setup these
+# if your adding this file locally, you probably want to uncomment these
+
 # DEBUG = True
 # TEMPLATE_DEBUG = True
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(settings.BASE_DIR, 'dev_db.sqlite3'),
+#         }
+# }
