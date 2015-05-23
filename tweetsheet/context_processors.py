0ca0247c19740fca604f808a49746602fdde376d
AJ <a@ajweb.eu>
2015-05-23 13:56:35 +0200

    Updated context processor to gather all credentials necessary for twitter

--- tweetsheet/context_processors.py
+++ tweetsheet/context_processors.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from social_auth.db.django_models import UserSocialAuth
 
 
@@ -5,15 +6,18 @@ def twitter_credentials(request):
     """
     Add to context the Twitter credentials for the authenticated user, if any.
     """
-    context = {}
+    context = {
+        'twitter_consumer_key': settings.TWITTER_CONSUMER_KEY,
+        'twitter_consumer_secret': settings.TWITTER_CONSUMER_SECRET
+    }
     if request.user.is_authenticated:
         try:
             user = UserSocialAuth.objects.get(user=request.user)
             tokens = user.extra_data.get('access_token')
             if tokens:
-                context['token_secret'] = tokens.split('&')[0].split('=')[1]
-                context['token'] = tokens.split('&')[1].split('=')[1]
+                context['twitter_token_secret'] = tokens.split('&')[0].split('=')[1]
+                context['twitter_token'] = tokens.split('&')[1].split('=')[1]
         except UserSocialAuth.DoesNotExist:
-            context['token_secret'] = ''
-            context['token'] = ''
+            context['twitter_token_secret'] = ''
+            context['twitter_token'] = ''
     return context
