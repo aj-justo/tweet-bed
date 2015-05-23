a4139efa8e2315564dd65c698273ccb2eae02772
AJ <a@ajweb.eu>
2015-05-23 13:19:12 +0200

    Removed twitter credentials settings file

--- twitter_credentials_template.py
+++
@@ -1,6 +0,0 @@
-# rename to twitter_settings.py and fill in your twitter oAuth credentials
-# ONCE RENAMED, DO NOT INCLUDE THE FILE IN VERSION CONTROL
-# you can better set it up on the server once you deploy
-# TODO ask for these credentials when deploying
-TWITTER_CONSUMER_KEY = '7qRaL3u4e3HY7k2R6rIs6Vb8r'
-TWITTER_CONSUMER_SECRET = '4gAuqnp6VEVhxHj770zPGmm1Ij2MLxIxYSzNbO5OPtXu2TiZCk'
